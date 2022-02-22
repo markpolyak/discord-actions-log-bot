c0e440ee62495b71341cbb441dca2b6e841d0800
Mark Polyak <markpolyak@gmail.com>
2022-02-22 21:21:30 +0300

    Read BOT_TOKEN from env variable

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 # Discord bot token. Can be retrieved at https://discord.com/developers/applications/
 # by creating a new bot for an application.
-BOT_TOKEN = 'TOKEN'
+import os
+BOT_TOKEN = os.environ['BOT_TOKEN']
 
 # Log channel name
 LOG_CHANNEL = 'лог-посещений'
 
 # Channel to listen to for command
-COMMAND_CHANNEL = 'commands'
+COMMAND_CHANNEL = 'посещаемость'
 
 # Role to which bot is allowed to respond to
 # TODO: Make it a list
