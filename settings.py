be0d961cf4bd3a689afb96e243d2d094cfa38bac
Dron <theprivetandreybest@yandex.ru>
2022-06-18 17:06:27 +0300

    Merge two settings file to one

--- settings.py
+++ settings.py
@@ -11,10 +11,31 @@ COMMAND_CHANNEL = 'посещаемость'
 
 # Role to which bot is allowed to respond to
 # TODO: Make it a list
-ALLOWED_ROLE = 'staff'
+ALLOWED_ROLE = 'admin'
 
 # Bot name that makes a log
 LOGGING_BOT = 'Dyno'
 
 # Min time in minutes for setting attendance
-MIN_TIME_DELTA=65
+MIN_TIME_DELTA=60
+
+# path to crefential file
+GOOGLE_CREDENTIALS_FILE = 'credentials.json'
+
+# path to token.pickle
+GOOGLE_TOKEN_PICKLE='token.pickle'
+
+# name of part, where attendance in google sheet - on 0 zero row!
+NAME_OF_ATTENDANCE = 'Посещаемость лекций'
+
+# row, where start name attendance (from 1)
+ROW_START_NAME_ATTENDANCE=1
+
+# row, where start date (from 1)
+ROW_START_Date_ATTENDANCE=2
+
+# col where start FIOs (from 1)
+COL_START_FIOS=2
+
+# row where start FIOs (from 1)
+ROW_START_FIOS=3
